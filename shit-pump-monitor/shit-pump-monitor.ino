2ab7d2a40d7bf722ea56ef7e30061f25b6ceaf46
Jarrett Thacker <thejart@gmail.com>
2022-04-22 00:13:46 -0400

    second, also shitty commit

--- shit-pump-monitor/shit-pump-monitor.ino
+++ shit-pump-monitor/shit-pump-monitor.ino
@@ -10,13 +10,17 @@
  */
 #include <SPI.h>
 #include <WiFiNINA.h>
-#include <avr/dtostrf.h>
 
 #include "arduino_secrets.h" 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int status = WL_IDLE_STATUS;     // the WiFi radio's status
+
+char webserver[] = "irk.evergreentr.com";
+
+//WiFiSSLClient client;
+WiFiClient client;
 
 void setup() {
   //Initialize serial and wait for port to open:
@@ -26,44 +30,46 @@ void setup() {
   }
 
   initializeWifi();
-  // you're connected now, so print out the data:
-  Serial.print("You're connected to the network");
+  
+  Serial.println("You're connected to the network");
   printCurrentNet();
   printWifiData();
-
-
 }
 
 void loop() {
   httpCallout();
-  
-  // check the network connection once every 10 seconds:
-  //delay(10000);
-  //printCurrentNet();
 }
 
-void httpCallout() {
-  HttpClient jartclint;
-
-  Serial.println("Requesting some robots...");
-  client.get("http://irk.evergreentr.com/poop.txt");
-  
-  // if there are incoming bytes available
-  // from the server, read them and print them:
-  while (client.available()) {
-    char c = client.read();
-    SerialUSB.print(c);
+// WiFi Stuff
+void initializeWifi() {
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
   }
-  SerialUSB.flush();
 
-  delay(5000);
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
 }
 
 void printWifiData() {
   // print your board's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
-  Serial.println(ip);
   Serial.println(ip);
 
   // print your MAC address:
@@ -75,8 +81,8 @@ void printWifiData() {
 
 void printCurrentNet() {
   // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+  //Serial.print("SSID: ");
+  //Serial.println(WiFi.SSID());
 
   // print the MAC address of the router you're attached to:
   byte bssid[6];
@@ -109,27 +115,33 @@ void printMacAddress(byte mac[]) {
   Serial.println();
 }
 
-void initializeWifi() {
-  // check for the WiFi module:
-  if (WiFi.status() == WL_NO_MODULE) {
-    Serial.println("Communication with WiFi module failed!");
-    // don't continue
+// HTTP Stuff
+void httpCallout() {
+  Serial.println("\nStarting connection to web server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(webserver, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("GET /poop.txt HTTP/1.1");
+    client.println("Host: irk.evergreentr.com");
+    client.println("Connection: close");
+    client.println();
+  }
+  
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+
+    // do nothing forevermore:
     while (true);
-  }
-
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
-
-  // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
-    delay(10000);
   }
 }
