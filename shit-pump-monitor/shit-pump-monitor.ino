c2064b08c696be6d8664739fb23e840c4fd5b892
Jarrett Thacker <thejart@gmail.com>
2022-05-09 17:42:00 -0400

    add a Nano 33 IoT-specific timing correction

--- shit-pump-monitor/shit-pump-monitor.ino
+++ shit-pump-monitor/shit-pump-monitor.ino
@@ -36,7 +36,10 @@ bool gyroDebug = false;
 bool httpDebug = false;
 
 unsigned long oneMinute = 60000; // 60k milliseconds
-unsigned long twelveHours = 43200000; // 43,200 seconds * 1000 milliseconds
+// The Nano 33IoT's clock frequency can change depending on power source due to it not having a crystal-based clock.
+// In my experience this results in a clock that runs ~0.1% slower than advertised
+// https://forum.arduino.cc/t/nano-33-iot-millis-rate-varies-with-usb-power-source/939392/2
+unsigned long twelveHours = 43200000 - 44085; // 43,200 seconds * 1000 milliseconds (minus ~0.1% per the above comment)
 unsigned long startTimeMark;
 
 WiFiSSLClient client;
